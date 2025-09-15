dd31a6f1d559e723d21b0bc413409e86030850ab
Nicolly <saantos.nick1606@gmail.com>
2025-09-15 15:55:16 -0300

    feat: permitir busca de artistas por país de origem

--- queries/02-select.sql
+++ queries/02-select.sql
@@ -3,3 +3,6 @@ SELECT * FROM artistas;
 SELECT COUNT (*) AS total_artistas FROM artistas;
 
 SELECT nome,genero_musical FROM artistas;
+
+SELECT nome,genero_musical FROM artistas
+WHERE pais_origem = 'Brasil';
