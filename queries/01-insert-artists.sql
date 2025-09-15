ca3996500338d22d9f85d3153f273240de83f9c1
Nicolly <saantos.nick1606@gmail.com>
2025-09-15 15:47:54 -0300

    feat: implementando filtros SQL para seleção de dados específicos na tabela artistas

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -10,3 +10,19 @@ CREATE TABLE artistas(
     musica_famosa VARCHAR(50),
     ativo BOOLEAN
 );
+
+INSERT INTO artistas(nome,tipo,membros,ano_lancamento,pais_origem,musica_famosa,ativo);
+VALUES
+('Adele','Solo',1,'Pop',2006,'Reino Unido','Rolling in the Deep',TRUE),
+('21 Pilots','Banda',2,'Rock Alternativo',2009,'Estados Unidos','Stressed Out',TRUE),
+('Lana del Rey','Solo',1,'Pop',2010,'Estados Unidos','Summertime Sadness',TRUE),
+('Ed Sheeran', 'Solo', 1, 'Pop', 2011, 'Reino Unido', 'Shape of You', TRUE),
+('Dua Lipa', 'Solo', 1, 'Pop', 2015, 'Reino Unido', 'Levitating', TRUE),
+('Maroon 5', 'Banda', 5, 'Pop Rock', 2002, 'Estados Unidos', 'Sugar', TRUE),
+('Shakira', 'Solo', 1, 'Pop', 1995, 'Colômbia', 'Waka Waka', TRUE),
+('Justin Bieber', 'Solo', 1, 'Pop', 2009, 'Canadá', 'Baby', TRUE),
+('The Weeknd', 'Solo', 1, 'R&B', 2011, 'Canadá', 'Save Your Tears', TRUE),
+('Bruno Mars', 'Solo', 1, 'Pop', 2010, 'Estados Unidos', 'Grenade', TRUE),
+('Katy Perry', 'Solo', 1, 'Pop', 2008, 'Estados Unidos', 'Firework', TRUE),
+('OneRepublic', 'Banda', 5, 'Pop Rock', 2007, 'Estados Unidos', 'Counting Stars', TRUE),
+('Anitta', 'Solo', 1, 'Pop', 2013, 'Brasil', 'Bang', TRUE);
---
+++ queries/02-select.sql
@@ -0,0 +1,5 @@
+SELECT * FROM artistas;
+
+SELECT COUNT (*) AS total_artistas FROM artistas;
+
+SELECT nome,genero_musical FROM artistas;
